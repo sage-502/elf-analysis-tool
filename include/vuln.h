c4de9f15cc3b5014a82a6c05ca1ae49fb34f1ab0
yeon-w <koyeonwoo1104@gmail.com>
2026-04-08 20:27:27 +0900

    Refactor vuln_t structure and update vuln_hit_t

--- include/vuln.h
+++ include/vuln.h
@@ -3,21 +3,20 @@
 
 #include "elf_parser.h"
 
-typedef struct s_vuln_hit
-{
-    char name[64];
-    char category[64];
-    char severity[16];
-    char table_name[32];
-} vuln_hit_t;
+#define MAX_VULN 32
 
 typedef struct s_vuln
 {
+    int has_gets;
+    int has_strcpy;
+    int has_rwx_segment;
+
     int count;
-    vuln_hit_t hits[128];
+    char *messages[MAX_VULN];
 } vuln_t;
 
 vuln_t analyze_vulnerability(elf_t *elf);
 void print_vuln(vuln_t v);
+void free_vuln(vuln_t *v);
 
 #endif
