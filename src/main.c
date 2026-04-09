63a395ea8fb3f161b89bd9257276cc569ea54ab2
sage <jjhcat0402@gmail.com>
2026-04-09 19:20:18 +0900

    Implement ELF parsing and vulnerability analysis

--- src/main.c
+++ src/main.c
@@ -1,1 +1,22 @@
+# include <stdio.h>
 
+# include "common.h"
+# include "elf_parser.h"
+# include "mitigation.h"
+# include "vuln.h"
+
+
+int main(int argc, char **argv)
+{
+    elf_t *elf = parse_elf(argv[1]);
+
+    mitigation_t m = analyze_mitigation(elf);
+
+    vuln_t v = analyze_vulnerability(elf);
+
+    print_vuln(v);
+
+    free_elf(elf);
+
+    return 0;
+}
